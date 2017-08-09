5f7b2a45a3a322060c2fde112539f5a4af0d7967
Tudor Ioan Marin <tudor_marin10@yahoo.com>
2017-08-09 23:00:07 +0300

    fix

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, `${__frontend}/index.html`));
 });
 
-const port = '22';
+const port = '80';
 app.set('port', port);
 
 const server = http.createServer(app);

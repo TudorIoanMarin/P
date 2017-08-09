2060526c1f7d65d7b8da62440898a5200e0dc178
Tudor Ioan Marin <tudor_marin10@yahoo.com>
2017-08-09 22:17:13 +0300

    Second commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const     express = require('express'),
+         path = require('path'),
+         http = require('http'),
+  serveStatic = require('serve-static'),
+   bodyParser = require('body-parser');
+
+const __frontend = '../dist';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, __frontend)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, `${__frontend}/index.html`));
+});
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`API running on localhost: ${port}`));
